67c70af8862ab7b3bb77ed5ee9f4ceef4ad11e82
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-18 22:31:55 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -22,7 +22,7 @@ def play(songs)
   else 
     is_int = Integer(choice)
     if (is_int != nil)
-      if (choice > 0) && choice < songs.length + 1
+      if (choice > 0) && (choice < (songs.length + 1))
         puts "hi"
       end
     else
